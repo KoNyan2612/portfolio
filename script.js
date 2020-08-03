0c1bbc14f43b9469cc6fc1fc47638349e3608176
KoNyan2612 <nyanlynnhtut@gmail.com>
2020-08-03 12:42:16 +0630

    added projects

--- script.js
+++ script.js
@@ -2,36 +2,43 @@ let themeDots = document.getElementsByClassName('theme-dot');
 
 let theme = localStorage.getItem('theme');
 
-if(theme == null){
+if (theme == null) {
     setTheme('light')
-}else{
+} else {
     setTheme(theme)
 }
 
-for (var i=0; themeDots.length > i; i++){
-    themeDots[i].addEventListener('click', function(){
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function () {
         var mode = this.dataset.modeTheme;
         console.log(mode, 'clicked theme');
         setTheme(mode)
     })
 }
 
-function setTheme(mode){
-    if(mode == 'light'){
+function setTheme(mode) {
+    if (mode == 'light') {
         document.getElementById('theme-style').href = 'default.css'
     }
 
-    if(mode == 'blue'){
+    if (mode == 'blue') {
         document.getElementById('theme-style').href = 'blue.css'
     }
 
-    if(mode == 'green'){
+    if (mode == 'green') {
         document.getElementById('theme-style').href = 'green.css'
     }
 
-    if(mode == 'purple'){
+    if (mode == 'purple') {
         document.getElementById('theme-style').href = 'purple.css'
     }
 
     localStorage.setItem('theme', mode)
+}
+
+let submit = document.getElementById('submit-btn');
+submit.addEventListener("click", failAlert);
+
+function failAlert() {
+    alert("Contact form is currently not available. Please contact me by email or phone. Thank you.")
 }
